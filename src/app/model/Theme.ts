01d792dcea413d0bce6278ee08dffff603670aa1
QuentinRo <quentin.rossier@cpnv.ch>
2019-06-19 16:32:55 +0200

    new model Theme

---
+++ src/app/model/Theme.ts
@@ -0,0 +1,10 @@
+export class Theme {
+
+    constructor(public id: number, public name: string, public description: string) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+}
+
