06190c6c556c9a3439efbbfa9867e58925423ddc
QuentinRo <quentin.rossier@cpnv.ch>
2019-06-20 17:25:49 +0200

    Modul camera added (and some maj)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
 import {IonicStorageModule} from '@ionic/storage'
-
+import { Camera } from '@ionic-native/camera/ngx';
 import {IonicModule, IonicRouteStrategy, ToastController} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
@@ -15,12 +15,13 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(),HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     DataProvider,
+    Camera,
     ToastController,
     HttpClient
   ],
