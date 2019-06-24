4e0143e9d30a43ede6befee2525e2408c7bda5e1
QuentinRo <quentin.rossier@cpnv.ch>
2019-06-24 12:14:42 +0200

    add champ
    speed, quality, pleaseant

--- src/app/model/Job.ts
+++ src/app/model/Job.ts
@@ -2,7 +2,8 @@ export class Job {
     // tslint:disable-next-line:max-line-length
 
     constructor(public id: number, public title: string, public description: string, public theme: string,
-                public date: string, public duration: number, public karmapoints: number, public image: string, public owner: number, public worker: number, public status_id: number) {
+                public date: string, public duration: number, public karmapoints: number, public image: string,
+                public owner: number, public worker: number, public status_id: number, public speed: number, public quality: number, public pleaseant: number) {
         this.id = id;
         this.title = title;
         this.description = description;
@@ -14,6 +15,9 @@ export class Job {
         this.owner = owner;
         this.worker = worker;
         this.status_id = status_id;
+        this.speed = speed
+        this.quality = quality
+        this.pleaseant = pleaseant
     }
     public getTitle() {
         console.log(this.title);
